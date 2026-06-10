3a4563766d3c80376e9c4cd4da1d16f24e8b56a1
alokkumar45 <alokku981870@gmail.com>
2026-06-10 17:03:25 +0530

    fix production cookies

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,16 +1,21 @@
-export const sendToken = (user, statusCode, res,message)=> {
+export const sendToken = (user, statusCode, res, message) => {
     const token = user.getJWTToken();
+
     const options = {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
     };
-    res.status(statusCode).cookie('token', token, options).json({
-        success: true,
-        message,
-        user,
-        token,
 
-    })
-} 
+    res.status(statusCode)
+        .cookie("token", token, options)
+        .json({
+            success: true,
+            message,
+            user,
+            token,
+        });
+};
